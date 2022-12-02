4f24f42ac93c490f984e10e0454d93c5fe05f239
SON <wjdgh2777@naver.com>
2022-12-03 01:58:33 +0900

    Refactor: 코드 재구성

--- src/controllers/menu.js
+++ src/controllers/menu.js
@@ -6,6 +6,9 @@ const model = require('../utils/connect');
 const user_job = model.sequelize.models.user_job;
 const user_characteristic = model.sequelize.models.user_characteristic;
 
+const service = require('../services/menu');
+const { keySelectWhere, parseValue, parseIds, jobUserFind, educationUserFind, careerUserFind, userMentoFilter } = service;
+
 const { Op } = require('sequelize');
 
 exports.searchMentoT = async (req, res) => {
@@ -73,170 +76,47 @@ exports.searchMentoT = async (req, res) => {
     let set = new Set(ids);
     ids = [...set];
 
-    if(ids.length === 0) {
-        return res.status(400).json({ message: "No data" });
-    } else {
-        User.findAll({
-            where: {[Op.and]: [
-                { position: "mentoo" },
-                { id: {[Op.or]: ids} },
-            ]},
-            order: [['createdAt', 'DESC']],
-        }).then((data) => {
-            return res.status(200).json({ data: data });
-        }).catch((err) => {
-            return res.status(500).json({ err });
-        });
-    }
+    if(ids.length === 0) return res.status(400).json({ message: "No data" });
+    else userMentoFilter(ids).then((data) => {
+        return res.status(200).json({ data: data });
+    }).catch((err) => {
+        return res.status(500).json({ err });
+    });
 };
 
 exports.searchMentoB = async (req, res) => {
-    let keywords = req.query; // job, education, career, company, university
-    let keys = Object.keys(keywords), keyword = [];
-    
-    for (let i = 0; i < keys.length; i++) {
-        let key = keys[i];
-        keyword.push(keywords[key].split(','));
-    }
+    let { job, university, education, company, career} = req.query;
+    let keywords = req.query;
+    let keys = Object.keys(keywords);
+    let value = parseValue(keywords, keys);
 
     let id, ids = [];
     let where_education, where_career = null;
     
-    let a = (a,b,ids) => {
-        let arr = [{ userkey: {[Op.or]: ids}}];
-        let temp = [];
-        for(let x of keys) {
-            if(x === a || x === b) temp.push(x);
-        }
-        console.log(temp);
-        console.log(keyword[keys.indexOf('education')])
-        for(let x = 0; x < temp.length; x++) {
-            arr.push({ [temp[x]] : {[Op.or]: keyword[keys.indexOf(temp[x])]}})
-        }
-        console.log(arr);
-        return {[Op.and]: arr}
-    }
-    
-    if(req.query.job) {
-        id = await Job.findAll({
-            attributes: ['id'],
-            where: { job: {[Op.or]: keyword[keys.indexOf('job')]}},
-            order: [['createdAt', 'DESC']],
-        }).then(async (data) => {
-            let job_id = [];
-            for(let x = 0; x < data.length; x++) {
-                job_id.push(data[x].id);
-            }
-            
-            return await user_job.findAll({
-                attributes: ['UserId'],
-                where: { JobId: {[Op.or]: job_id}},
-                order: [['createdAt', 'DESC']],
-            }).catch((err) => {
-                return res.status(500).json({ err });
-            });
-        }).catch((err) => {
-            return res.status(500).json({ err });
-        });
-        
-        for(let x = 0; x < id.length; x++) {
-            ids.push(id[x].UserId);
-        }
+    if(job) {
+        id = await jobUserFind(value, keys);
+        for(let idx = 0; idx < id.length; idx++) ids.push(id[idx].UserId);
     } 
-    
-    console.log(ids);
-    
-    where_education = a('education', 'university',ids);
-    console.log(where_education);
-    if (ids.length !== 0 && (req.query.education || req.query.university)) {
-        id = await Education.findAll({
-            attributes: ['userkey'],
-            where: where_education,
-            order: [['createdAt', 'DESC']],
-        }).then(async (data) => {
-            console.log(data);
-            if(data.length === 0) return;
-            
-            let education_id = [];
-            for(let x = 0; x < data.length; x++) {
-                education_id.push(data[x].userkey);
-            }
-            
-            return await User.findAll({
-                attributes: ['id'],
-                where: { id : {[Op.or]: education_id}},
-                order: [['createdAt', 'DESC']],
-            }).catch((err) => {
-                return res.status(500).json({ err });
-            });
-        }).catch((err) => {
-            return res.status(500).json({ err });
-        });
-        
-        if(id !== undefined) {
-            ids = [];
-            for(let x = 0; x < id.length; x++) {
-                ids.push(id[x].id);
-            }
-        } else {
-            ids = [];
-        }
-    }
-    
-    console.log(ids);
-    
-    where_career = a('career','company',ids);
-    if (ids.length !== 0 && (req.query.career || req.query.company)) {
-        id = await Career.findAll({
-            attributes: ['userkey'],
-            where: where_career,
-            order: [['createdAt', 'DESC']],
-        }).then(async (data) => {
-            console.log(data);
-            if(data.length === 0) return;
-            
-            let career_id = [];
-            for(let x = 0; x < data.length; x++) {
-                career_id.push(data[x].userkey);
-            }
-            
-            return await User.findAll({
-                attributes: ['id'],
-                where: { id : {[Op.or]: career_id}},
-                order: [['createdAt', 'DESC']],
-            }).catch((err) => {
-                return res.status(500).json({ err });
-            });
-        }).catch((err) => {
-            return res.status(500).json({ err });
-        });
 
-        if(id !== undefined) {
-            ids = [];
-            for(let x = 0; x < id.length; x++) {
-                ids.push(id[x].id);
-            }
-        } else {
-            ids = [];
-        }
+    where_education = keySelectWhere('education', 'university',ids, value, keys);
+    if (ids.length !== 0 && (education || university)) {
+        id = await educationUserFind(where_education);
+        ids = parseIds(id);
     }
 
-    let set = new Set(ids);
+    where_career = keySelectWhere('career', 'company',ids, value, keys);
+    if (ids.length !== 0 && (career || company)) {
+        id = await careerUserFind(where_career);
+        ids = parseIds(id);
+    }
+
+    let set = new Set(ids); //. 중복 제거
     ids = [...set];
     
-    if(ids.length === 0) {
-        return res.status(400).json({ message: "No data" });
-    } else {
-        User.findAll({
-            where: {[Op.and]: [
-                { position: "mentoo" },
-                { id: {[Op.or]: ids} },
-            ]},
-            order: [['createdAt', 'DESC']],
-        }).then((data) => {
-            return res.status(200).json({ data: data });
-        }).catch((err) => {
-            return res.status(500).json({ err });
-        });
-    }
+    if(ids.length === 0) return res.status(400).json({ message: "No data" });
+    else userMentoFilter(ids).then((data) => {
+        return res.status(200).json({ data: data });
+    }).catch((err) => {
+        return res.status(500).json({ err });
+    });
 }
--- src/services/menu.js
+++ src/services/menu.js
@@ -1,1 +1,155 @@
 'use strict';
+
+const { User, Job, Characteristic, Education, Career } = require('../utils/connect');
+
+const model = require('../utils/connect');
+const user_job = model.sequelize.models.user_job;
+const user_characteristic = model.sequelize.models.user_characteristic;
+
+const { Op } = require('sequelize');
+
+/**
+ * 사용자로부터 입력받은 쿼리 속 키들의 값들을 리턴하는 함수
+ *
+ * @param {*} keywords 사용자로부터 입력받은 쿼리
+ * @param {*} keys 쿼리 속 키값 들
+ * @returns
+ */
+exports.parseValue = (keywords, keys) => {
+    let value = [];
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        value.push(keywords[key].split(','));
+    } return value;
+}
+
+/**
+ * key 값과 key의 value 값들을 파싱 하여, 받은 key에 대해서만,
+ * sql where 절을 생성하는 함수
+ *
+ * @param {*} a education, university, company, career
+ * @param {*} b education, university, company, career
+ * @param {*} ids job && a && b 기준으로 필터링 된 유저 id들
+ * @param {*} value 사용자로부터 입력받은 key들의 value 값들
+ * @param {*} keys 사용자로부터 입력받은 key 값들
+ * @returns 
+ */
+exports.keySelectWhere = (a, b, ids, value, keys) => {
+    let where_arr = [{ userkey: {[Op.or]: ids}}];
+    let type = [];
+    for(let key of keys) if(key === a || key === b) type.push(key);
+    for(let idx = 0; idx < type.length; idx++) {
+        where_arr.push({ [type[idx]] : {[Op.or]: value[keys.indexOf(type[idx])]}})
+    } return {[Op.and]: where_arr}
+}
+
+/**
+ * 필터링 후, 공통되는 id에 관해 ids를 만드는 함수
+ * 
+ * @param {*} id 필터링 후, 공통된 id
+ * @returns 
+ */
+exports.parseIds = (id) => {
+    if(id !== undefined) {
+        let ids = []; // 다음 for 문에서 공통된 부분에 대해서만 넣어하기 때문에 []
+        for(let idx = 0; idx < id.length; idx++) ids.push(id[idx].id);
+        return ids;
+    } else return []; // job을 받고서, 서로 공통된 부분이 없다는 것 = ids 제거
+}
+
+/**
+ * 해당하는 job을 가지는 user id를 리턴하는 함수
+ * 
+ * @param {*} value 사용자로부터 입력받은 key들의 value 값들
+ * @param {*} keys 사용자로부터 입력받은 key 값들
+ * @returns 
+ */
+exports.jobUserFind = async (value, keys) => {
+    return await Job.findAll({
+        attributes: ['id'],
+        where: { job: {[Op.or]: value[keys.indexOf('job')]}},
+    }).then(async (data) => {
+        let job_id = [];
+        for(let x = 0; x < data.length; x++) job_id.push(data[x].id);
+        
+        return await user_job.findAll({
+            attributes: ['UserId'],
+            where: { JobId: {[Op.or]: job_id}},
+        }).catch((err) => {
+            return res.status(500).json({ err });
+        });
+    }).catch((err) => {
+        return res.status(500).json({ err });
+    });
+}
+
+/**
+ * 해당하는 education or university를 가지는 user id를 리턴하는 함수
+ * 
+ * @param {*} where_education education 전용 where절 삽입
+ * @returns 
+ */
+exports.educationUserFind = async (where_education) => {
+    return await Education.findAll({
+        attributes: ['userkey'],
+        where: where_education,
+    }).then(async (data) => {
+        if(data.length === 0) return; // 데이터가 없을때, userkey에 접근하면 오류
+        
+        let education_id = [];
+        for(let x = 0; x < data.length; x++) education_id.push(data[x].userkey);
+        
+        return await User.findAll({
+            attributes: ['id'],
+            where: { id : {[Op.or]: education_id}},
+        }).catch((err) => {
+            return res.status(500).json({ err });
+        });
+    }).catch((err) => {
+        return res.status(500).json({ err });
+    });
+}
+
+/**
+ * 해당하는 company or career를 가지는 user id를 리턴하는 함수
+ * 
+ * @param {*} where_career career 전용 where절 삽입
+ * @returns 
+ */
+exports.careerUserFind = async (where_career) => {
+    return await Career.findAll({
+        attributes: ['userkey'],
+        where: where_career,
+    }).then(async (data) => {
+        if(data.length === 0) return;
+        
+        let career_id = [];
+        for(let x = 0; x < data.length; x++) career_id.push(data[x].userkey);
+        
+        return await User.findAll({
+            attributes: ['id'],
+            where: { id : {[Op.or]: career_id}},
+        }).catch((err) => {
+            return res.status(500).json({ err });
+        });
+    }).catch((err) => {
+        return res.status(500).json({ err });
+    });
+}
+
+/**
+ * 필터링 된 유저 id들을 받아, 중복 요소 제거 후,
+ * 그중 멘토인 유저를 출력하는 함수
+ *
+ * @param {*} ids 중복 요소가 제거된 필터링 후 유저 id들
+ * @returns 
+ */
+exports.userMentoFilter = (ids) => {
+    return User.findAll({
+        where: {[Op.and]: [
+            { position: "mentoo" },
+            { id: {[Op.or]: ids} },
+        ]},
+        order: [['createdAt', 'DESC']],
+    })
+}
