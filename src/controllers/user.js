aded0ffd033b34860b7d8f4bf718b0ac50067670
SON <wjdgh2777@naver.com>
2022-12-22 22:35:51 +0900

    Feat: 멘토 등록

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// const service = require('../services/menu');
+// const { } = service;
+
+const { User, Career } = require('../utils/connect');
+const { success, fail } = require('../functions/responseStatus');
+
+exports.mentorRegistration = async (req, res) => {
+    let { career, company } = req.body;
+    let username = req.session.sid
+
+    let data = await User.findOne({
+        attributes: ['id'],
+        where: { username: username },
+    })
+
+    await User.update({
+        position: 'mentor',
+    }, {
+        where: { id: data.id },
+    });
+    
+    Career.create({
+        career: career,
+        company: company,
+        userkey: data.id,
+    }).then(() => {
+        return success(res, 200, 'success.');
+    }).catch(err => {
+        return fail(res, 500, err);
+    });
+}
