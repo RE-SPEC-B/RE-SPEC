4eb9fba5f326691bd2cb69b61adef21ea8083b4f
SON <wjdgh2777@naver.com>
2022-12-30 00:16:19 +0900

    Comment: 주석 추가

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -8,6 +8,13 @@ const {
 
 const { success, fail } = require('../functions/responseStatus');
 
+/**
+ * 멘티에서 멘토를 등록하는 API입니다.
+ * 
+ * 프로세스)
+ * 1. 유저명을 통한 해당 유저 포지션 변경 & 유저 ID 반환
+ * 2. 반환된 유저 ID를 통하여, 해당 유저 Job, Career, Education 추가
+ */
 exports.mentorRegistration = async (req, res) => {
     let { job, company, career, university, education } = req.body;
     let username = req.session.sid;
--- src/services/user.js
+++ src/services/user.js
@@ -5,6 +5,13 @@ const { User, Job ,Career, Education } = require('../utils/connect');
 const model = require('../utils/connect');
 const user_job = model.sequelize.models.user_job;
 
+/**
+ * 유저명을 통하여, 해당 유저 포지션 변경 & 유저 ID를 반환합니다.
+ * 이미 해당 유저가 멘토일 경우 0을 반환합니다.
+ * 
+ * @param {*} username 현재 로그인중인 사용자의 유저명
+ * @returns 
+ */
 exports.userFindAndUpdate = (username) => {
     return User.findOne({
         attributes: ['id', 'position'],
@@ -19,6 +26,17 @@ exports.userFindAndUpdate = (username) => {
     })
 }
 
+/**
+ * 멘토로 갱신되는 멘티의 유저 ID를 받아, 해당 유저 Job, Career, Education 부분 추가
+ * 
+ * @param {*} job 생성할 해당 유저의 직업
+ * @param {*} user_id 멘토로 갱신되는 멘티의 유저 ID
+ * @param {*} career 생성할 해당 유저의 경력
+ * @param {*} company 생성할 해당 유저의 기업 규모
+ * @param {*} university 생성할 해당 유저의 대학교
+ * @param {*} education 생성할 해당 유저의 학력
+ * @returns 
+ */
 exports.jobFindAndInfoCreate = async (job, user_id, career, company, university, education) => {
     let job_data = await Job.findOne({
         attributes: ['id'],
