fb259996e2af8d50df7e97afc5cbcc853100e63e
SON <wjdgh2777@naver.com>
2023-01-31 11:40:43 +0900

    Edit: Swagger 재구성

--- src/controllers/data.js
+++ src/controllers/data.js
@@ -10,5 +10,5 @@ const { success, fail } = require('../functions/responseStatus');
 
 exports.enumValue = async (req, res) => {
     let data = await enumValueGet();
-    return success(res, 200, 'success.', data);
+    return success(res, 200, 'Get enum data success.', data);
 }
