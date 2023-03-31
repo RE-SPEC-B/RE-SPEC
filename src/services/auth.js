a9b32e3c52f76236b1f93a522b685944191c9ebf
Dev.SoNB <wjdgh2777@naver.com>
2023-03-31 10:19:32 +0900

    Edit: 변수명 통일 - auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -9,17 +9,17 @@ const _bcrypt = require('bcrypt');
  * 사용자로부터 입력받은 정보들을 바탕으로, 비밀번호 암호화 진행 후,
  * 회원가입 로직을 수행하는 함수
  *
- * @param {*} username 유저닉네임
+ * @param {*} user_name 유저닉네임
  * @param {*} email 유저이메일
  * @param {*} password 비밀번호
  * @param {*} provider 로그인 제공처
  * @returns
  */
-exports.register = async (username, email, password, provider) => {
+exports.register = async (user_name, email, password, provider) => {
     try {
         const hash_password = await _bcrypt.hash(password, 12);
         return await User.create({
-            username: username,
+            username: user_name,
             email: email,
             password: hash_password,
             provider: provider,
@@ -33,13 +33,13 @@ exports.register = async (username, email, password, provider) => {
  * 사용자로부터 입력받은 정보들을 바탕으로, 중복되는 유저가 있는지
  * 확인하고 반환하는 함수
  *
- * @param {*} username 유저닉네임
+ * @param {*} user_name 유저닉네임
  * @param {*} email 유저이메일
  * @returns
  */
-exports.userFind = async (username, email) => {
+exports.userFind = async (user_name, email) => {
     try {
-        return await User.findOne({ where: { [Op.or]: [{ username: username }, { email: email }] } });
+        return await User.findOne({ where: { [Op.or]: [{ username: user_name }, { email: email }] } });
     } catch (err) {
         throw new Error(err);
     }
