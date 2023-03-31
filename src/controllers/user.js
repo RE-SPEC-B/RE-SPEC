903551971a68f4140fbe1b88faa5d2065c114f36
Dev.SoNB <wjdgh2777@naver.com>
2023-03-31 10:48:47 +0900

    Edit: 변수명 통일 - user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,45 +1,23 @@
 'use strict';
 
 const user = require('../services/user');
-const { 
-    userFindAndUpdate, 
-    jobFindAndInfoCreate, 
-    mentorInfoGet, 
-    mentorReviewsGet, 
-    userInfoPut 
+
+const {
+    mentorInfoGet,
+    mentorReviewsGet,
+    userFindAndUpdate,
+    jobFindAndInfoCreate,
 } = user;
 
 const { success, fail } = require('../functions/responseStatus');
 
 /**
- * 멘티에서 멘토를 등록하는 API입니다.
- *
- * 프로세스)
- * 1. 유저명을 통한 해당 유저 포지션 변경 & 유저 ID 반환
- * 2. 반환된 유저 ID를 통하여, 해당 유저 Job, Career, Education 추가
- */
-exports.mentorRegistration = async (req, res) => {
-    let { job, company, companysize, career, university, education } = req.body;
-    let username = req.session.sid;
-
-    let user_id = await userFindAndUpdate(username);
-    if (user_id === 0) return fail(res, 403, 'You are already mentor.');
-
-    let result = await jobFindAndInfoCreate(job, user_id, career, company, companysize, university, education);
-    if (result === 'success.') {
-        return success(res, 200, 'Mentor register success.');
-    } else {
-        return fail(res, 500, result);
-    }
-};
-
-/**
  * 유저id를 받고, 멘토정보를 가져오는 API입니다.
  */
 exports.mentorInfo = (req, res) => {
-    const { userid } = req.params;
+    const user_id = req.params.userid;
 
-    mentorInfoGet(userid)
+    mentorInfoGet(user_id)
         .then((data) => {
             return success(res, 200, 'Get mentor info success.', data);
         })
@@ -56,16 +34,42 @@ exports.mentorInfo = (req, res) => {
  * 유저id를 받고, 멘토 리뷰들을 가져오는 API입니다.
  */
 exports.mentorReviews = (req, res) => {
-    const { userid } = req.params;
+    const user_id = req.params.userid;
 
-    mentorReviewsGet(userid)
+    mentorReviewsGet(user_id)
         .then((data) => {
             return success(res, 200, 'Get mentor reviews success.', data);
         })
         .catch((err) => {
-            return fail(res, 500, err);
+            if (err.message === 'There is no data.') {
+                return fail(res, 404, err.message);
+            } else {
+                return fail(res, 500, err.message);
+            }
         });
 };
+
+/**
+ * 멘티에서 멘토를 등록하는 API입니다.
+ *
+ * 프로세스)
+ * 1. 유저명을 통한 해당 유저 포지션 변경 & 유저 ID 반환
+ * 2. 반환된 유저 ID를 통하여, 해당 유저 Job, Career, Education 추가
+ */
+// exports.mentorRegistration = async (req, res) => {
+//     let { job, company, companysize, career, university, education } = req.body;
+//     let username = req.session.sid;
+
+//     let user_id = await userFindAndUpdate(username);
+//     if (user_id === 0) return fail(res, 403, 'You are already mentor.');
+
+//     let result = await jobFindAndInfoCreate(job, user_id, career, company, companysize, university, education);
+//     if (result === 'success.') {
+//         return success(res, 200, 'Mentor register success.');
+//     } else {
+//         return fail(res, 500, result);
+//     }
+// };
 
 // exports.userInfo = async (req, res) => {
 //     let username = req.session.sid, profile;
