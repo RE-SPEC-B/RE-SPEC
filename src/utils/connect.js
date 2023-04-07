85ad2f1cf70762aa0968b681ffcb1205fc7c3d73
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-04-08 00:01:48 +0900

    Feat: 쿼리 DB에 로깅
    * 쿼리 로깅
    * 사이즈가 작은 프로젝트이기때문에 파일시스템이 아닌 DB에 로깅 결정
    * 프로젝트가 발전하면 추후에 로깅 방식 변경 고려

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -12,10 +12,30 @@ const sequelize = new _sequelize(
     {
         host: _config.get('mysql_local.host'),
         dialect: _config.get('mysql_local.dialect'),
-        logging: _config.get('server.state') === 'production' ? false : console.log,
+        logging: (message) => {
+            console.log(message);
+            // 사이즈가 작은 프로젝트이기때문에 파일시스템이 아닌 DB에 로깅 결정
+            // 프로젝트가 발전하면 추후에 로깅 방식 변경 고려
+            if (!message.includes('logs')) {
+                logToDatabase(message);
+            }
+        },
         timezone: _config.get('mysql_local.timezone'), // Asia/Seoul
     },
 );
+
+// 로그 테이블 생성
+const Log = sequelize.define('log', {
+    message: {
+        type: _sequelize.STRING,
+        allowNull: false,
+    },
+});
+
+// DB에 로그 저장
+const logToDatabase = (message) => {
+    Log.create({ message });
+};
 
 Object.values(models).forEach((model) => model.init(sequelize));
 Object.values(models)
